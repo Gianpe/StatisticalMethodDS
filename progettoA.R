d8335ba412da178032ce4f81a6710b9ecfb9c7e3
Gianpe <57147997+Pepheight@users.noreply.github.com>
2021-08-20 09:56:48 +0200

    Ho ordinato il codice

--- progettoA.R
+++ progettoA.R
@@ -17,21 +17,12 @@ summary(aida) #poco leggibile
 missing <- sapply(aida, function(x) sum(is.na(x)))
 sort(missing)
 
-
+#clean and transform dataset
 aida = aida[!is.na(aida$`ATECO 2007code`),]
 aida = aida[!is.na(aida$`Number of employeesLast avail. yr`),]
 aida = aida[!is.na(aida$`Legal form`),]
 aida = aida[!is.na(aida$`Incorporation year`),]
-
 aida = aida[!aida$`Incorporation year`>aida$`Last accounting closing date`,]
-
-
-# summaries
-table(aida$`Registered office address - Region`)
-table(aida$`Legal status`)
-table(aida$`Legal form`)
-
-
 aida$"Legal status" = as.character(aida$"Legal status")
 aida$"Legal status"[aida$"Legal status"=="Bankruptcy"] = "Failed"
 aida$"Legal status"[aida$"Legal status"=="Dissolved"] = "Failed"
@@ -42,33 +33,30 @@ aida$"Legal status"[aida$"Legal status"=="Dissolved (merger)"] = "Failed"
 aida$"Legal status"[aida$"Legal status"=="In liquidation"] = "Failed"
 aida$"Legal status"[aida$"Legal status"=="Active (default of payments)"] = "Failed"
 aida$"Legal status"[aida$"Legal status"=="Active (receivership)"] = "Failed"
-table(aida$"Legal status")
-View(aida)
-
 aida$"Size"[aida$"Number of employeesLast avail. yr"<=10] = "Micro"
 aida$"Size"[aida$"Number of employeesLast avail. yr">10 & aida$"Number of employeesLast avail. yr"<=50 ] = "Small"
 aida$"Size"[aida$"Number of employeesLast avail. yr">50] = "Medium"
-table(aida$Size)
-View(aida)
-table(aida$`Last accounting closing date`)
-
-
 aida$"Age"[aida$"Legal status"== "Active"]= 2019- aida$"Incorporation year"
 aida$"Age"[aida$"Legal status"== "Failed"]= aida$"Last accounting closing date"- aida$"Incorporation year"
+
+
+# summaries
+table(aida$`Registered office address - Region`)
+table(aida$`Legal status`)
+table(aida$`Legal form`)
+
 
 table(aida$Age)
 
 
 
-
+#Comparison between active and failed companies
 active= subset(aida ,subset=( aida$"Legal status"=="Active")) 
 failed= subset(aida ,subset=( aida$"Legal status"!="Active"))
 
-plot(density(active$Age), main="EtÃ  aziende", xlab="EtÃ ", ylab="N di aziende") 
+plot(density(active$Age), main="Età aziende", xlab="Età", ylab="N di aziende") 
 lines(density((failed$Age)), col="red") 
-legend(78, 0.057, legend= c("Active", "Failed"), col= c("black", "red"), lty=1, cex=0.8 )
-hist(na.omit(active$Age), main="EtÃ  aziende", xlab="EtÃ ", ylab="N di aziende")
-hist(na.omit(failed$Age), main="EtÃ  aziende", xlab="EtÃ ", ylab="N di aziende")
+legend(50, 0.037, legend= c("Active", "Failed"), col= c("black", "red"), lty=1, cex=0.8 )
 
 bins = sort(unique(c(active$Age, failed$Age)))
 N1i = sapply(bins, function(i) sum(active$Age ==i))
